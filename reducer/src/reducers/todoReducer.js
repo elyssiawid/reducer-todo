72a87049ccd0c623f946127bb47c14e649608f83
Elyssia Widjaja <Elyssia@Elyssias-MBP.lan1>
2019-08-13 16:18:39 -0700

    reached MVP

--- reducer/src/reducers/todoReducer.js
+++ reducer/src/reducers/todoReducer.js
@@ -1,21 +1,28 @@
-export const initialState = {
-  item: "Learn about reducers",
-  completed: false,
-  id: Date.now()
-};
+//initial state needs to be an array
+export const initialState = [
+  {
+    item: "Learn about reducers",
+    completed: false,
+    id: Date.now()
+  }
+];
 export const todoReducer = (state, action) => {
   switch (action.type) {
-    case "TOGGLE_EDITING":
-      return {
-        ...state,
-        editing: !state.editing
-      };
-    case "UPDATE_TITLE":
-      return {
-        ...state,
-        title: action.payload,
-        editing: !state.editing
-      };
+    case "TOGGLE_ITEM":
+      state.map(item => {
+        if (item.id === action.payload) {
+          return {
+            ...item,
+            completed: !item.completed
+          };
+        } else {
+          return item;
+        }
+      });
+    case "ADD_TODO":
+      //this case needs to add a whole new todo object
+      console.log("add payload", action.payload);
+      return [...state, action.payload];
     default:
       return state;
   }
